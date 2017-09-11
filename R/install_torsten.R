cf2bef1e595e78b37ef6b0a62d529a374262857c
Jonathan Sidi <yonicd@gmail.com>
2017-09-11 16:56:32 -0400

    update install_torsten

--- R/install_torsten.R
+++ R/install_torsten.R
@@ -1,33 +1,72 @@
-install_torsten <- function(StanHeaders_version="2.16.0-1",
-                            rstan_version='2.16.2',
-                            branch=c('master','develop'),
+#' @title Installation for torsten
+#' @description installation of torsten with rstan, that replaces stanHeaders.
+#' @param StanHeaders_version package_version, package version of StanHeaders to append Torsten Default: NULL
+#' @param rstan_version package_version, package version of rstan to install, Default: NULL
+#' @param branch character, install the current build ('master') or different
+#' development branch of torsten (e.g. 'develop'), Default: 'master'
+#' @param lib character, giving the library directory where to install the packages, Default: .libPaths()[1]
+#' @param ... parameters to pass to install.packages
+#' @details installation will replace the 'StanHeaders/include/src/stan' and 'StanHeaders/include/stan' of stanHeaders and install
+#' source rstan without dependencies.
+#' @rdname install_torsten
+#' @export
+
+install_torsten <- function(StanHeaders_version=NULL,
+                            rstan_version=NULL,
+                            branch='master',
                             lib=.libPaths()[1],
-                            destdir = NULL) {
-  ## Download and edit StanHeaders (version 2.16)
-  install.packages(sprintf("https://cran.r-project.org/src/contrib/StanHeaders_%s.tar.gz",StanHeaders_version),
+                            ...) {
+
+  thiswd <- getwd()
+
+  install_headers <- FALSE
+
+  if(is.null(StanHeaders_version)){
+    if(c('StanHeaders')%in%row.names(installed.packages())){
+      StanHeaders_version <- packageVersion('StanHeaders')
+    }else{
+      StanHeaders_version <- pkgVersionCRAN('StanHeaders')
+      install_headers <- TRUE
+    }
+  }
+
+  if(install_headers)
+    install.packages(sprintf("https://cran.r-project.org/src/contrib/StanHeaders_%s.tar.gz",StanHeaders_version),
                    repos = NULL,
                    type = "source",
                    lib=lib,...)
-  setwd('lib')
-  system("git clone https://github.com/metrumresearchgroup/stan.git")
-  setwd("stan")
 
-  system(sprintf("git checkout torsten-%s",branch))  # comment out to get torsten-master
-  setwd('..')
-  system("rm -rf StanHeaders/include/src/stan")
-  system("mv stan/src/stan StanHeaders/include/src/stan")
-  system("rm -rf stan")
-  system("git clone https://github.com/metrumresearchgroup/math.git")
-  setwd("math")
-  system(sprintf("git checkout torsten-%s",branch))  # comment out to get torsten-master
-  setwd('..')
-  system("rm -rf StanHeaders/include/stan")
-  system("mv math/stan StanHeaders/include/stan")
-  system("rm -rf math")
+  if(is.null(rstan_version)) rstan_version <- pkgVersionCRAN('rstan')
+
+  td <- tempdir()
+
+  setwd(td)
+
+  for(repo in c('stan','math')){
+    system(sprintf("git clone https://github.com/metrumresearchgroup/%s.git",repo))
+    setwd(repo)
+
+    if(branch!='master') system(sprintf("git checkout torsten-%s",branch))
+    setwd('..')
+
+    switch(repo,
+           stan={
+             system(sprintf("rm -rf %s/StanHeaders/include/src/stan",lib))
+             system(sprintf("mv %s/stan/src/stan %s/StanHeaders/include/src/stan",td,lib))
+           },
+           math={
+             system(sprintf("rm -rf %s/StanHeaders/include/stan",lib))
+             system(sprintf("mv %s/math/stan %s/StanHeaders/include/stan",td,lib))
+           })
+
+    system(sprintf("rm -rf %s",file.path(td,repo)))
+  }
+
+  setwd(thiswd)
 
   install.packages(sprintf("https://cran.r-project.org/src/contrib/rstan_%s.tar.gz",rstan_version),
                    repos = NULL,
                    type = "source",
-                   lib=lib
-                   )
+                   lib=lib,...)
+
 }
